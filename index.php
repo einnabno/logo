994e64c7cad6d92ce505105f8c18c0c5190bace8
einnabno <70646362+einnabno@users.noreply.github.com>
2020-09-09 21:46:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+	<body>
+		<div class="pizza"></div>
+			<div class="salami"></div>
+			<div class="salami1"></div>
+			<div class="salami2"></div>
+			<div class="salami3"></div>
+			<div class="salami4"></div>
+			<div class="salami5"></div>
+			<div class="salami6"></div>
+			<div class="salami7"></div>
+			<div class="salami8"></div>
+			<div class="salami9"></div>
+			<div class="pepperoni"></div>
+			<div class="title" id="top-title">YANNIE'S</div>
+			<div class="title" id="bottom-title">PIZZA</div>
+	</body>
+</html>
